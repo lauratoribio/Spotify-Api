720a9c0c681c09ae6fafbb1e3b8c41d0d3af1b4c
Laura Toribio <lsancheztoribio@hotmail.com>
2022-03-15 20:16:21 +0100

    account component styling

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,33 +1,45 @@
 import React, { useState, useEffect } from "react";
 import SideBar from "../../components/SideBar/SideBar";
 
-import apiClient from '../../spotify'
-
+import apiClient from "../../spotify";
 
 export default function Account() {
-  
-  const [userData, setUserData] = useState(null)
-  
+  const [userDataName, setUserDataName] = useState("");
+  const [userDataId, setUserDataId] = useState("");
+  const [userDataFollowers, setUserDataFollowers] = useState("");
+  const [userDataImage, setUserDataImage] = useState("");
+
   useEffect(() => {
-    apiClient.get("me").then(response => {
-      setUserData(response.data)
-      console.table(response.data)
-    })
-  }, [])
+    apiClient.get("me").then((response) => {
+      setUserDataName(response.data.display_name);
+      console.log(response.data.display_name);
+
+      setUserDataId(response.data.id);
+      console.log(response.data.id);
+
+      setUserDataFollowers(response.data.followers.total);
+      console.log(response.data.followers.total);
+
+      setUserDataImage(response.data.images[0].url);
+      console.log(response.data.images[0].url);
+    });
+  }, []);
 
   return (
     <>
-     <SideBar />
-       <div className="user-card">
-           {userData?.map(user => 
-              <div>
-                 <h2>{user.display_name}</h2>
-                 <h3>{user.id}</h3>
-                 <p>Followers: {user.followers.total}</p>
-                 <img src={user.images[0].url} alt=""/>
-              </div>
-           )}
-      </div> 
+      <SideBar />
+      <div className="account-container">
+        <div className="user-card">
+          <img className="user-card__image" src={userDataImage} alt="" />
+          <div className="user-card__details">
+            <h2 className="user-card__details__title">{userDataName}</h2>
+            <h3 className="user-card__details__subtitle">{userDataId}</h3>
+            <p className="user-card__details__p">
+              Followers: <span>{userDataFollowers}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
